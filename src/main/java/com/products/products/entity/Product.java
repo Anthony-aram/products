d2ff27fab5b877106618c3da9e33d3fead30a515
Anthony-aram <aram.anthony@gmail.com>
2023-02-26 15:16:09 +0100

    Add Product entity

---
+++ src/main/java/com/products/products/entity/Product.java
@@ -0,0 +1,42 @@
+package com.products.products.entity;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.Size;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.time.LocalDateTime;
+import java.util.Set;
+
+@Entity
+@Table(
+        name = "Products",
+        uniqueConstraints = { @UniqueConstraint(name = "UQ_Products_Title", columnNames = { "title" }) }
+)
+public class Product {
+    @Id
+    @GeneratedValue(
+            strategy = GenerationType.IDENTITY
+    )
+    private int id;
+    @Column(nullable = false)
+    @Size(min = 3, max = 30, message = "Le titre doit être entre 3 et 30 caractères")
+    private String title;
+    @Column(nullable = false)
+    @Size(min = 10, max = 1000)
+    private String description;
+    @Column(nullable = false)
+    private Float price;
+    private Float discountPercentage = 0F;
+    @Min(0) @Max(5)
+    private Float rating;
+    @Column(nullable = false)
+    private Integer stock = 0;
+    private Set<String> images;
+    @CreationTimestamp
+    private LocalDateTime dateCreated;
+    @UpdateTimestamp
+    private LocalDateTime lastUpdated;
+}
