4fe906c592e576427ab5e16db673917309961ace
Anthony-aram <aram.anthony@gmail.com>
2023-04-22 16:29:06 +0200

    Ajout du cache sur la référence Role

--- src/main/java/com/products/products/ProductsApplication.java
+++ src/main/java/com/products/products/ProductsApplication.java
@@ -5,8 +5,10 @@ import io.swagger.v3.oas.annotations.info.Info;
 import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
+@EnableCaching
 @OpenAPIDefinition(
 		info = @Info(title = "Products Project API", version = "1.0.0"),
 		servers = {@Server(url = "http://localhost:8080")})
--- src/main/java/com/products/products/repository/RoleRepository.java
+++ src/main/java/com/products/products/repository/RoleRepository.java
@@ -1,10 +1,21 @@
 package com.products.products.repository;
 
 import com.products.products.entity.Role;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
 
 /**
  * Repository pour la table roles
  */
 public interface RoleRepository extends JpaRepository<Role, String> {
+
+    /**
+     * Récupération de la liste des rôles
+     * @return La liste des rôles
+     */
+    @Override
+    @Cacheable(value = "roles")
+    List<Role> findAll();
 }
